40e0777db6390db2f7b4396518d7aca87b030a4f
Craig Martek <craigmartek@gmail.com>
2022-01-21 23:56:20 -0500

    popups and such

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { HttpClient } from '@angular/common/http';
 import { Subscription, timer } from 'rxjs';
 import { DateTime } from 'luxon';
 
+interface Activity {
+  id: number,
+  name: string,
+  date: string,
+  distanceMi: number,
+  polyline: L.Polyline,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,6 +29,8 @@ export class AppComponent implements AfterViewInit, OnDestroy {
   lastUpdated?: string;
   craigFaceX = '50px';
   flipCraigFace = false;
+
+  private allActivities: Activity[] = [];
 
   private subscription?: Subscription;
 
@@ -41,18 +51,58 @@ export class AppComponent implements AfterViewInit, OnDestroy {
       this.craigFaceX = ((this.progressBarWidth * (this.totalMiles / this.targetMiles)) - 25) + 'px';
       this.totalMilesStr = this.totalMiles.toFixed(2);
       this.lastUpdated = DateTime.fromISO(stats['last_updated']).toLocaleString({ month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false, timeZoneName: 'short' });
-      for (const line of stats['polylines']) {
-        const polyline = L.polyline(decode(line)).addTo(this.map!);
-        polyline.on('mouseover', () => {
-          polyline.setStyle({ weight: 6 });
+      for (const activity of stats['activities']) {
+        const polyline = L.polyline(decode(activity['polyline'])).addTo(this.map!);
+        this.allActivities.push({
+          id: activity['id'],
+          name: activity['name'],
+          date: activity['date'],
+          distanceMi: activity['distance_mi'],
+          polyline: polyline,
         });
-        polyline.on('mouseout', () => {
-          polyline.setStyle({ weight: 3 });
+        polyline.on('mouseover', (e: any) => {
+          this.highlightNearbyPolylines(e.latlng as L.LatLng);
+        });
+        polyline.on('mouseout', (e) => {
+          this.resetPolylines();
         });
       }
       this.subscription = timer(1000, 1000).subscribe(() => {
         this.flipCraigFace = !this.flipCraigFace;
       });
+    });
+  }
+
+  private highlightNearbyPolylines(latLng: L.LatLng) {
+    const distances: [number, Activity][] = [];
+    const point = this.map!.latLngToLayerPoint(latLng);
+    for (const activity of this.allActivities) {
+      const closestPoint = activity.polyline.closestLayerPoint(point);
+      if (closestPoint) {
+        const distance = point.distanceTo(activity.polyline.closestLayerPoint(point));
+        distances.push([distance, activity]);
+      }
+    }
+    distances.sort((a, b) => a[0] - b[0]);
+
+    const lines = [];
+    for (const d of distances) {
+      if (d[0] < 5) {
+        d[1].polyline.setStyle({ weight: 6 });
+        lines.push(`<li>${d[1].date}: <a href="https://www.strava.com/activities/${d[1].id}" target="_blank">${d[1].name}</a> - ${d[1].distanceMi.toFixed(2)} mi</li>`);
+      } else {
+        break;
+      }
+    }
+    // what a dumb way of getting these in chronological order
+    lines.sort();
+    L.popup().setLatLng(latLng).setContent(`<ul style="list-style-type: none; padding: 0; margin: 0;">${lines.join('')}</ul>`).openOn(this.map!);
+  }
+
+  private resetPolylines() {
+    this.map?.closePopup();
+    this.allActivities.forEach((a) => {
+      a.polyline.setStyle({ weight: 3 });
     });
   }
 
